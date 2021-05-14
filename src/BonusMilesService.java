fd5c0d984810ed21c2e67e5c77fc32cc5816696d
Orlov-D <82273356+Orlov-D@users.noreply.github.com>
2021-05-14 23:52:04 +0300

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,11 +1,7 @@
 public class BonusMilesService {
     public int calculate(int cost) {
         int bonus;
-        if (cost % 20 == 0) {
-            bonus = cost / 20;
-        } else {
-            bonus = (cost - cost % 20) / 20;
-        }
+        bonus = cost / 20;
         return bonus;
     }
 }
